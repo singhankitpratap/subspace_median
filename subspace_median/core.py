c42b73290046eed2ebf81c131b94f805cf69bf5c
singhankitpratap <78484221+singhankitpratap@users.noreply.github.com>
2024-09-05 16:42:05 -0500

    Update core.py

--- subspace_median/core.py
+++ subspace_median/core.py
@@ -1,24 +1,23 @@
 import torch
 from torch.linalg import qr, eigh
-
+torch.set_default_dtype(torch.float64)
 def orthonormalize(U):
+    U = U.to(torch.float64)
     Q, R = qr(U)
     return Q
 
 def compute_projection_matrix(U):
+    U = U.to(torch.float64)
     return U @ U.T
 
-def approx_gm(P_list, T_gm, epsilon_gm):
-    return weiszfeld_algorithm(P_list, T_gm, epsilon_gm)
-
 def weiszfeld_algorithm(A, T_gm, epsilon_gm):
-    A = torch.stack(A)
-    L, n, m = A.shape
+    A = torch.stack(A).to(torch.float64)
+    L, d = A.shape
     z_t = A.mean(dim=0)
 
     for t in range(T_gm):
-        numer = torch.zeros_like(z_t)
-        denom = 0.0
+        numer = torch.zeros_like(z_t, dtype=torch.float64)
+        denom = torch.tensor(0.0, dtype=torch.float64)
 
         for i in range(L):
             dist = torch.norm(A[i] - z_t)
@@ -36,9 +35,13 @@ def weiszfeld_algorithm(A, T_gm, epsilon_gm):
 
     return z_t
 
+
 def subspace_median(U_list, T_gm, epsilon_gm):
-    U_list = [orthonormalize(U) for U in U_list]
-    P_list = [compute_projection_matrix(U) for U in U_list]
-    P_gm = approx_gm(P_list, T_gm, epsilon_gm)
-    l_best = torch.argmin(torch.tensor([torch.norm(P - P_gm) for P in P_list]))
+    U_list = [orthonormalize(U).to(torch.float64)  for U in U_list]
+    P_list = [compute_projection_matrix(U).to(torch.float64) for U in U_list]
+    P_list_flattened = [P.view(-1).to(torch.float64) for P in P_list]
+    P_gm_flat = weiszfeld_algorithm(P_list_flattened, T_gm, epsilon_gm)
+    P_gm = P_gm_flat.view(P_list[0].shape)
+    l_best = torch.argmin(torch.tensor([torch.norm(P - P_gm) for P in P_list], dtype=torch.float64))
     return U_list[l_best]
+
